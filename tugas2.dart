0bfc00dbc64accaf177c5e8c67e7c3865191bee4
citra cahyaningtias <citraca@outlook.co.id>
2026-02-09 09:13:42 +0700

    menentukan kelulusan mahasiswa, input dari user

---
+++ tugas2.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+void main () {
+  stdout.write("masukkan nilai uts: ");
+  int uts = int.parse(stdin.readLineSync()!);
+
+
+  stdout.write("masukkan nilai uas: ");
+  int uas = int.parse(stdin.readLineSync()!);
+  //meminta input dari pengguna
+
+stdout.write("masukkan presentase kehadiran: ");
+double kehadiran = double.parse(stdin.readLineSync()!);
+
+double ratarata = (uts+uas) / 2.0;
+
+bool lulus = 
+ratarata >= 70 &&
+kehadiran >= 75 && 
+(uts >= 60 && uas >= 60);
+
+// Output hasil
+  if (lulus) {
+    stdout.writeln("Mahasiswa LULUS");
+  } else {
+    stdout.writeln("Mahasiswa TIDAK LULUS");
+  }
+}
+
+
+
+
